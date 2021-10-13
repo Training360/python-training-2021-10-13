c1b4d6e2109600f9e8fe7aae6096da186ece2895
Istvan Viczian <viczian.istvan@gmail.com>
2021-10-13 14:16:29 +0200

    Módosítás

--- sum_numbers.py
+++ sum_numbers.py
@@ -1,7 +1,7 @@
 # Ez a program bekér két számot
 
 a_input = input("Add meg az elso szamot!") # ez bekéri a számot
-b_input = input("Add meg a masodik szamot!")
-a = int(a_input)
+b_input = input("Ird be a masodik szamot!")
+a = int(a_input) # Konvertálás
 b = int(b_input)
 print(a + b)
