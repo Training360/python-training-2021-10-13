0df9e39511121597787ee65edb82b7f4fa3d901f
Istvan Viczian <viczian.istvan@gmail.com>
2021-10-13 14:13:04 +0200

    Első Python programom

---
+++ sum_numbers.py
@@ -0,0 +1,7 @@
+# Ez a program bekér két számot
+
+a_input = input("Add meg az elso szamot!") # ez bekéri a számot
+b_input = input("Add meg a masodik szamot!")
+a = int(a_input)
+b = int(b_input)
+print(a + b)
